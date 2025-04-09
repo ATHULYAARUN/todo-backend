1572aea1ad85c89237b95f23b7c47e39a7c06b28
athulyaarun461 <athulyaarunb@gmail.com>
2025-04-09 21:16:56 +0530

    Fix connection string and set up backend routes

--- Server.js
+++ Server.js
@@ -5,17 +5,21 @@ const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://helanantony:Mongodb@cluster0.xhpe6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+// MongoDB connection
+mongoose.connect('mongodb+srv://athulyaarun2026:athulya12@cluster0.q2tcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+// API routes
 app.use('/api/todos', todoRoutes);
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
